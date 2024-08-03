20861504053f6cb5d9024c54cd325f04b6aa8a18
Carles Capellas <carles@peekvision.org>
2024-08-03 23:48:21 +0200

    Minor adjustments

--- src/otf-migrations/notes/schema-1-note-otf-migration.ts
+++ src/otf-migrations/notes/schema-1-note-otf-migration.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { OtfMigration } from '../otf-migration';
 
+/*
+- Previous model:
+ 
+  export class Note {
+    text: string;
+  }
+
+- Current model:
+ 
+  export class Note {
+    title: string;
+    description?: string;
+  }
+*/
+
 @Injectable()
 export class Schema1NoteOtfMigration extends OtfMigration {
   schemaTarget = 1;
 
   protected apply(body: any): void {
-    if (body.text) {
-      body.title = body.text;
-      delete body.text;
-    }
+    body.title = body.text;
+    delete body.text;
   }
 }
--- src/otf-migrations/notes/schema-2-note-otf-migration.ts
+++ src/otf-migrations/notes/schema-2-note-otf-migration.ts
@@ -1,6 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { OtfMigration } from '../otf-migration';
 
+/*
+- Previous model:
+ 
+  export class Note {
+    title: string;
+    description?: string;
+  }
+
+- Current model:
+ 
+  export class Note {
+    title: string;
+    description?: string;
+    text: boolean;
+  }
+*/
+
 @Injectable()
 export class Schema2NoteOtfMigration extends OtfMigration {
   schemaTarget = 2;
